0544f925a8fb62c1e3761b610640980b409728bd
RensDeWit <113677920+RensDeWit@users.noreply.github.com>
2022-09-16 12:00:46 +0200

    Add files via upload

---
+++ pong/Game1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Net;
+using System.Security.Cryptography.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace pong
+{
+    public class pong : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D ball;
+        Vector2 ballPosition = new Vector2(100, 100);
+
+        static void Main()
+        {
+            pong game = new pong();
+            game.Run();
+        }
+
+        public pong()
+        {
+            Content.RootDirectory = "Content";
+            graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ball = Content.Load<Texture2D>("bal");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            spriteBatch.Begin();
+            spriteBatch.Draw(ball, ballPosition, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ pong/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new pong.pong();
+game.Run();
